b13fdfe1354b5e9ecef83e5db9389f5101bea4e2
zyinghua <nz1019@iCloud.com>
2022-03-02 18:35:35 +1100

    Started login activity UI design in XML

--- app/src/main/java/com/example/food_product_comparison_android_app/LoginActivity.java
+++ app/src/main/java/com/example/food_product_comparison_android_app/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.food_product_comparison_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.food_product_comparison_android_app.Fragments.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.login_fragment, new LoginFragment()).addToBackStack("login_frag").commit();
     }
 }
