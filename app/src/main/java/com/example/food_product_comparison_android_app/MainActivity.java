d139538b98ad603225b4b10be0ce7316ae20328a
zyinghua <nz1019@iCloud.com>
2022-03-05 17:55:16 +1100

    updates. 05/03/2022

--- app/src/main/java/com/example/food_product_comparison_android_app/MainActivity.java
+++ app/src/main/java/com/example/food_product_comparison_android_app/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -17,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
+    private int login_option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +31,48 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         this.mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        this.login_option = getIntent().getIntExtra(LoginActivity.LOGIN_OPTION_TAG, -1);
 
         Button sign_out = findViewById(R.id.sign_out);
         sign_out.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.sign_out) {
-                    googleSignOut();
+                switch(login_option)
+                {
+                    case LoginActivity.FACEBOOK_LOGIN:
+                        facebookLogOut();
+                        break;
+
+                    case LoginActivity.GOOGLE_LOGIN:
+                        googleLogOut();
+                        break;
+
+                    default:
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        finish();
+                        break;
                 }
             }
         });
     }
 
-    private void googleSignOut() {
+    private void googleLogOut() {
         mGoogleSignInClient.signOut()
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        finish();
                         Toast.makeText(getApplicationContext(),"Logged Out",Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        finish();
                     }
                 });
+    }
+
+    private void facebookLogOut() {
+        LoginManager.getInstance().logOut();
+        Toast.makeText(getApplicationContext(),"Logged Out",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 
     private void google_revokeAccess() {
