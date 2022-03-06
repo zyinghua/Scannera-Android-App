0970e8956ed98dc2adefa3ad1b1bdf9246bedd0b
zyinghua <nz1019@iCloud.com>
2022-03-06 17:21:19 +1100

    Updates. 06/03/2022 - 17:21

---
+++ app/src/main/java/com/example/food_product_comparison_android_app/Fragments/PasswordResetEmailSentFragment.java
@@ -0,0 +1,20 @@
+package com.example.food_product_comparison_android_app.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.food_product_comparison_android_app.R;
+
+public class PasswordResetEmailSentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_reset_email_sent, container, false);
+    }
+}
