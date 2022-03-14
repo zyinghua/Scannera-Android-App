73c2510487ee60149348ab5c5e5ec33982381fef
zyinghua <nz1019@iCloud.com>
2022-03-14 12:45:02 +1100

    Changed rating star color | Added edittext animation on empty | added feedback icon to the entry button | added references to java classes

--- app/src/main/java/com/example/food_product_comparison_android_app/FeedbackActivity.java
+++ app/src/main/java/com/example/food_product_comparison_android_app/FeedbackActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.facebook.login.Login;
 
 public class FeedbackActivity extends AppCompatActivity {
     private ImageButton top_back_btn;
     private RatingBar rating_bar;
+    private EditText feedback_et;
+    private Button submit_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +33,34 @@ public class FeedbackActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float rating = rating_bar.getRating();
+                String feedback = feedback_et.getText().toString();
+
+                if (feedback.isEmpty())
+                {
+                    Animation shake = AnimationUtils.loadAnimation(FeedbackActivity.this, R.anim.shake);
+                    feedback_et.startAnimation(shake);
+                    Toast.makeText(FeedbackActivity.this, getString(R.string.feedback_empty), Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    // Store the rating and feedback here
+                    Toast.makeText(FeedbackActivity.this, getString(R.string.feedback_success), Toast.LENGTH_LONG).show();
+                    onBackPressed();
+                }
+            }
+        });
     }
 
     private void findViews()
     {
         this.top_back_btn = findViewById(R.id.top_back_btn);
         this.rating_bar = findViewById(R.id.about_us_rating_bar);
+        this.feedback_et = findViewById(R.id.feedback_et);
+        this.submit_btn = findViewById(R.id.submit_btn);
     }
 }
