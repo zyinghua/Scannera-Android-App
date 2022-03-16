fba1f5719bd0feafdd91ab2f75cfdcd0bd7b5c68
zyinghua <nz1019@iCloud.com>
2022-03-16 22:32:17 +1100

    Added a loading animation in one of the fragments of the scan history activity for online data retrieval

--- app/src/main/java/com/example/food_product_comparison_android_app/ScanHistoryActivity.java
+++ app/src/main/java/com/example/food_product_comparison_android_app/ScanHistoryActivity.java
@@ -3,8 +3,20 @@ package com.example.food_product_comparison_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.food_product_comparison_android_app.Fragments.LoadingFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ScanHistoryActivity extends AppCompatActivity {
     private ImageButton top_back_btn;
@@ -14,7 +26,18 @@ public class ScanHistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_history);
 
-        this.findViews();
+        if (savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.SH_content_container, new LoadingFragment()).commit();
+
+        this.top_back_btn = findViewById(R.id.top_back_btn);
+        ExecutorService singleExecutor = Executors.newSingleThreadExecutor();
+        Handler uiHandler = new Handler(Looper.getMainLooper());
+
+        String currentDate = new SimpleDateFormat("d MMM yyyy, EEE, HH:mm", Locale.getDefault()).format(new Date());
+        String time="15/03/2022, 09:15";
+
+        if (currentDate.compareTo(time) > 0)
+            Toast.makeText(this, currentDate, Toast.LENGTH_LONG).show();
 
         top_back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,10 +45,5 @@ public class ScanHistoryActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-    }
-
-    private void findViews()
-    {
-        this.top_back_btn = findViewById(R.id.top_back_btn);
     }
 }
