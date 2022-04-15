37fae6bb6f5f56f47a51204937ebfd9f0abf5ac1
zyinghua <yzho0149@student.monash.edu>
2022-04-15 21:40:13 +1000

    Implemented the home recommended product list API call.

--- app/src/main/java/com/example/food_product_comparison_android_app/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/food_product_comparison_android_app/Fragments/HomeFragment.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,11 +25,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.food_product_comparison_android_app.LoadingDialog;
 import com.example.food_product_comparison_android_app.Product;
 import com.example.food_product_comparison_android_app.ProductListRecyclerViewAdapter;
 import com.example.food_product_comparison_android_app.R;
+import com.example.food_product_comparison_android_app.StarredProductActivity;
 import com.example.food_product_comparison_android_app.User;
 import com.example.food_product_comparison_android_app.Utils;
 import com.facebook.GraphRequest;
@@ -43,19 +46,24 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
     private User user;
     private TextView welcome_username_tv;
     private CircularImageView home_user_img;
-    ArrayList<Object> products;
+    private List<Product> recommended_products;
     private LinearLayoutManager layoutManager;
     private RecyclerView homeRecyclerView;
     private ProductListRecyclerViewAdapter productListRecyclerViewAdapter;
-    private boolean isLoading;
+    // private boolean isLoading;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -116,13 +124,11 @@ public class HomeFragment extends Fragment {
 
     private void setUpContent()
     {
-        products = new ArrayList<>();
+        // isLoading = false;
         layoutManager = new LinearLayoutManager(getContext());
         homeRecyclerView.setLayoutManager(layoutManager);
-        productListRecyclerViewAdapter = new ProductListRecyclerViewAdapter(requireActivity().getApplicationContext(),getActivity(), products);
-        homeRecyclerView.setAdapter(productListRecyclerViewAdapter);
-        loadProductListContent();
-        isLoading = false;
+
+        handleOnGetRecommendedProducts(System.currentTimeMillis());
 
 //        homeRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
 //            @Override
@@ -149,9 +155,45 @@ public class HomeFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    private void loadProductListContent()
+    private void handleOnGetRecommendedProducts(Long init_time)
     {
+        LoadingDialog loading_dialog = new LoadingDialog(requireActivity());
+        loading_dialog.show();
 
+        Call<List<Product>> call = Utils.getServerAPI(requireActivity()).getStarredProducts(Utils.getLoggedUser(requireActivity()).getId());
+
+        call.enqueue(new Callback<List<Product>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Product>> call, @NonNull Response<List<Product>> response) {
+                loading_dialog.dismiss();
+
+                if (response.isSuccessful() && response.body() != null)
+                {
+                    recommended_products = response.body();
+
+                    productListRecyclerViewAdapter = new ProductListRecyclerViewAdapter(
+                            requireActivity().getApplicationContext(),getActivity(), new ArrayList<>(recommended_products));
+                    homeRecyclerView.setAdapter(productListRecyclerViewAdapter);
+                }
+                else
+                {
+                    if ((System.currentTimeMillis() - init_time) / 1000 < Utils.MAX_SERVER_RESPOND_SEC) {
+                        handleOnGetRecommendedProducts(init_time);
+                        Log.e("DEBUG", "Get Recommended products code: " + response.code());
+                    }
+                    else
+                    {
+                        Toast.makeText(requireActivity(), getString(R.string.general_error), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<Product>> call, @NonNull Throwable t) {
+                loading_dialog.dismiss();
+                Toast.makeText(requireActivity(), getString(R.string.general_error), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 //    private void loadASetOfProducts()
